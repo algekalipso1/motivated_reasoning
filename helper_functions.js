5bec83fdc0677338b5948cfb718c65e3f1125e01
algekalipso1 <andesgomez@dn0a22229e.sunet>
2014-05-24 17:38:23 -0700

    functional progresss barrr

--- helper_functions.js
+++ helper_functions.js
@@ -13,7 +13,7 @@ function initialsClick() {
     hide_and_seek("initials_slide", "loading_slide");
     numComplete = numComplete+1; 
     $('#trial-num').html(numComplete);
-    $('.bar').css('width', (200.0 * (1+numComplete)/10) + 'px');
+    $('.bar').css('width', (200.0 * (1+numComplete)/15) + 'px');
     //for testing
     setTimeout(connectionLoad, 1000);
     //for real runs
@@ -26,7 +26,7 @@ function connectionLoad() {
     hide_and_seek("loading_slide", "loading_slide2");
     numComplete = numComplete+1; 
     $('#trial-num').html(numComplete);
-    $('.bar').css('width', (200.0 * (1+numComplete)/10) + 'px');
+    $('.bar').css('width', (200.0 * (1+numComplete)/15) + 'px');
     //for testing
     setTimeout(conditionLoad, 1000);
     //for real runs
@@ -37,7 +37,7 @@ function conditionLoad() {
     hide_and_seek("loading_slide2", "condition_slide");
     numComplete = numComplete+1; 
     $('#trial-num').html(numComplete);
-    $('.bar').css('width', (200.0 * (1+numComplete)/10) + 'px');
+    $('.bar').css('width', (200.0 * (1+numComplete)/15) + 'px');
     experiment.instructions_function();
 }
 
@@ -53,7 +53,7 @@ function evClick(condition) {
             hide_and_seek("evidence_slide", "eval_slide");
             numComplete = numComplete+1; 
             $('#trial-num').html(numComplete);
-    		$('.bar').css('width', (200.0 * (1+numComplete)/10) + 'px');
+    		$('.bar').css('width', (200.0 * (1+numComplete)/15) + 'px');
             DW_strength_raw = new Dragdealer('simple-slider1', {'x': 0.5});
         }
     };
@@ -78,7 +78,7 @@ function evClick(condition) {
             hide_and_seek("evidence_slide", "eval_slide");
             numComplete = numComplete+1; 
             $('#trial-num').html(numComplete);
-		    $('.bar').css('width', (200.0 * (1+numComplete)/10) + 'px');
+		    $('.bar').css('width', (200.0 * (1+numComplete)/15) + 'px');
             DW_strength_raw = new Dragdealer('simple-slider1', {'x': 0.5});
         }
     };
@@ -130,7 +130,7 @@ function evalClick() {
 	        optimism_raw.disable();
 	        numComplete = numComplete+1; 
 	        $('#trial-num').html(numComplete);
-		    $('.bar').css('width', (200.0 * (1+numComplete)/10) + 'px');
+		    $('.bar').css('width', (200.0 * (1+numComplete)/15) + 'px');
 	        hide_and_seek("eval_slide", "manip_slide");
         }
     }     
@@ -140,7 +140,7 @@ function evalClick() {
 function manipClick() {
     numComplete = numComplete+1; 
     $('#trial-num').html(numComplete);
-    $('.bar').css('width', (200.0 * (1+numComplete)/10) + 'px');
+    $('.bar').css('width: 200px');
     hide_and_seek("manip_slide", "debrief_slide");              
 }
 
@@ -263,7 +263,7 @@ function hand_HTML(base, n, prop_mat, props, id, position) {
 
     html += '<img  src="images3/hand.png" width=50px height=50px'  + 
      'alt="it is the hand!" id="objHandProperty"/>';
-    //'alt="' + props[n] + '" ' + 'id="' + id + 'Property' + String(c+10) + '"/>';
+    //'alt="' + props[n] + '" ' + 'id="' + id + 'Property' + String(c+15) + '"/>';
     return html;
 }
 
