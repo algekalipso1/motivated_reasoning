b2d322a7260b16060f9da18f657a54819ba9cda4
algekalipso1 <andesgomez@rescomp-13-221757.stanford.edu>
2014-06-03 11:03:53 -0700

    some images, old

--- analysis.R
+++ analysis.R
@@ -26,7 +26,10 @@ agrci <- function(x){
 
 
 
+#first_batch = read.csv("csv/motivation_results_one_point_five.csv",header=TRUE, sep="\t")
 first_batch = read.csv("csv/motivation_results_study_2.csv",header=TRUE, sep="\t")
+
+
 head(first_batch)
 colnames(first_batch)
 
@@ -77,6 +80,7 @@ first_batch$evidence_compliant = (first_batch$Answer.evidence_condition == 1 & f
 first_batch$competition_condition_compliant = (first_batch$Answer.competition_condition == 1 & first_batch$Answer.DW_check == '"partner"') |
   (first_batch$Answer.competition_condition == 2 & first_batch$Answer.DW_check == '"opponent"') |
   (first_batch$Answer.competition_condition == 0 & first_batch$Answer.DW_check == '"neither"')
+  #(first_batch$Answer.competition_condition == 0 & first_batch$Answer.DW_check == '"opponent"')
 
 
 first_batch$compliant = first_batch$evidence_compliant & first_batch$competition_condition_compliant
@@ -130,10 +134,15 @@ summary(glm(Answer.DW_strength ~ as.factor(Answer.competition_condition) + Answe
 # Excluding the control condition:
 summary(glm(Answer.DW_strength ~  Answer.evidence_condition + Answer.expectation_of_winning + as.factor(Answer.competition_condition) + Answer.strength_of_average_player + Answer.role_of_luck_in_game , data = first_batch_DW_relevant))
 
-hist(first_batch_c$Answer.DW_strength)
 
 
+hist(first_batch_c$Answer.motivation_to_win, main = "Motivation to win the game", xlab = "Motivation slider value", ylab = "count")
+hist(first_batch$Answer.DW_strength, breaks = 9, main = "DW strength distribution - Pilot", xlab = "Strength slider value", ylab = "count")
 
+hist(first_batch_c$Answer.expectation_of_winning, breaks = 14)
+
+summary(aov(Answer.motivation_to_win ~ Answer.expectation_of_winning, data = first_batch_c))
+summary(glm(Answer.DW_strength ~ Answer.motivation_to_win + Answer.expectation_of_winning + as.factor(Answer.competition_condition) , data = first_batch_c))
 
 
 
